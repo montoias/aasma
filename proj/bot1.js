ead19477b004984d893cfb79335d2574a8213e97
Ana Rita da Costa Pereira <ist169899@rnlmaster.rnl.ist.utl.pt>
2014-04-05 19:44:53 +0100

    Adicionada parte do move

---
+++ proj/bot1.js
@@ -0,0 +1,29 @@
+var mineflayer = require('../');
+var vec3 = mineflayer.vec3;
+var 
+var bot = mineflayer.createBot({
+  username: "GrupolindoAASMA",
+  viewDistance: "normal",
+});
+
+bot.on('move', function() {
+	bot.clearControlStates();
+	var moves = ['forward', 'back', 'right', 'left'];
+	var rand = math.random()%3;
+	bot.setControlState(moves[rand], true);
+	if (true) {};
+
+});
+
+
+bot.on('health', function() {
+  bot.chat("I have " + bot.health + " health and " + bot.food + " food");
+
+});
+
+bot.on('entityHurt', function(entity) {
+	bot.attack(entity);
+}); 
+
+
+
