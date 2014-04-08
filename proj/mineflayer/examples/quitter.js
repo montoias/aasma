d2593c1a7d980f6bf8461c37e2c37640bed73173
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2014-04-08 13:48:02 +0100

    Fix project structure.

--- proj/mineflayer/examples/quitter.js
+++
@@ -1,9 +0,0 @@
-var mineflayer = require('../');
-var bot = mineflayer.createBot({
-  username: "emobot",
-});
-
-bot.once('spawn', function() {
-    bot.chat('Goodbye, cruel world!');
-    bot.quit();
-});
