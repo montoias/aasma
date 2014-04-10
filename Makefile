a3b51aba578cd0f0dc9405d5bda8269301c036ce
Ana Rita Pereira <anarita@MacBook-Pro-de-Ana.local>
2014-04-10 01:06:50 +0100

    trying the movement of the reactibebot. right now it just move in circles, somthing like that.

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
-
-all:
-	unzip proj/deps/Nodus.zip
-	mv -f proj/deps/Nodus /Library/Application\ Support/minecraft/versions
-
 npm:
 	cd proj/; \
 	npm install; \
@@ -11,3 +6,4 @@ npm:
 	git clone https://github.com/andrewrk/mineflayer; \
 	cd mineflayer; \
 	git checkout -b a35a2d83d37890c402cd8a3ba4bb17803a2f6807; \
+	npm install; \
--- proj/botReactive.js
+++ proj/botReactive.js
@@ -2,26 +2,52 @@ var mineflayer = require('mineflayer');
 var vec3 = mineflayer.vec3;
 
 var bot = mineflayer.createBot({
-  username: "GrupolindoAASMA",
-  viewDistance: "normal",
+  username: "rtjiia"
+});
+
+bot.on('spawn', function(){
+	bot.setControlState('forward', true);
+	bot.entity.yaw = 0;
 });
 
 bot.on('move', function() {
-	bot.clearControlStates();
-	var moves = ['forward', 'back', 'right', 'left'];
-	bot.setControlState(moves[randomIntInc(0,3)], true);
-});
+	var botposition = bot.entity.position.floored();
+	var block = bot.blockAt(getNextBlock(bot.entity.yaw, botposition));
 
+	//console.log(block);
+	if(block && block.boundingBlock != 'empty'){
+		var moves = [0, Math.PI, Math.PI/2, (Math.PI *3)/2];
+		bot.entity.yaw = moves[randomIntInc(0,3)];
+	}
+});
 
 bot.on('health', function() {
   bot.chat("I have " + bot.health + " health and " + bot.food + " food");
-
 });
 
-bot.on('entityHurt', function(entity) {
-	bot.attack(entity);
-}); 
+// bot.on('entityHurt', function(entity) {
+// 	bot.attack(entity);
+// }); 
 
 function randomIntInc (low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+function getNextBlock (yaw, position) {
+	switch(yaw) {
+		case 0:
+			position.x = position.x + 1;
+			break;
+		case Math.PI/2:
+			position.y = position.y + 1;
+			break;
+		case Math.PI:
+			position.x = position.x - 1;
+			break;
+		case (Math.PI * 3)/2:
+			position.y = position.y - 1;
+			break; 
+	}
+	console.log(position);
+	return position;
 }
