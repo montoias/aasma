8f56eb2d6807c41f3349df6a6676bcd61983587b
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2014-04-11 01:58:11 +0100

    Minecraft bot running around reacting to the environment

--- proj/botReactive.js
+++ proj/botReactive.js
@@ -1,25 +1,31 @@
 var mineflayer = require('mineflayer');
 var vec3 = mineflayer.vec3;
+var EventEmitter = require('events').EventEmitter
 
 var bot = mineflayer.createBot({
-  username: "rtjiia"
+  username: "master"
 });
+
 
 bot.on('spawn', function(){
 	bot.setControlState('forward', true);
-	bot.entity.yaw = 0;
 });
 
-bot.on('move', function() {
-	var botposition = bot.entity.position.floored();
-	var block = bot.blockAt(getNextBlock(bot.entity.yaw, botposition));
+bot.on('entityMoved', function () {
+	var botposition = bot.entity.position;
+	var neighbors = freeNeighbors(botposition);
 
-	//console.log(block);
-	if(block && block.boundingBlock != 'empty'){
-		var moves = [0, Math.PI, Math.PI/2, (Math.PI *3)/2];
-		bot.entity.yaw = moves[randomIntInc(0,3)];
-	}
-});
+	console.log(neighbors)
+	if(neighbors.length > 0 && !isYawValid(bot.entity.yaw, botposition)){
+		var elem = neighbors[randomIntInc(0,(neighbors.length - 1))];
+
+		var lookAtY = botposition.y + bot.entity.height;
+	 	var lookAtPoint = vec3(botposition.x + elem.x, lookAtY, botposition.z + elem.z);
+	 	bot.lookAt(lookAtPoint);
+	 }
+
+}
+);
 
 bot.on('health', function() {
   bot.chat("I have " + bot.health + " health and " + bot.food + " food");
@@ -29,9 +35,85 @@ bot.on('health', function() {
 // 	bot.attack(entity);
 // }); 
 
+function isYawValid (yaw, pos) {
+	var n = freeNeighbors(pos);
+	if(n.length > 0){
+		var direction = getVecFromYaw(yaw);
+		return isValidDirection (n, direction);		
+	}
+	return false;
+}
+
+function isValidDirection (vec, elem) {
+	var result = false;
+	vec.forEach( function  (e) {
+		if (e.x == elem.x && e.z == elem.z)
+			result = true;
+	});
+	return result;	
+}
+
+var unit = [
+    vec3(-1, 0,  0), // north
+    vec3( 1, 0,  0), // south
+    vec3( 0, 0, -1), // east
+    vec3( 0, 0,  1), // west
+]; 
+
+function getVecFromYaw (yaw) {
+	yaw = checkYaw(yaw);
+	if (yaw >= (Math.PI *3)/2) 
+		return unit[1];
+	if (yaw >= Math.PI) 
+		return unit[3];
+	if (yaw >= (Math.PI/2)) 
+		return unit[0];
+	if (yaw >= 0)  
+		return unit[2];
+}
+
+function checkYaw (yaw) {
+	console.log(yaw)
+	yaw = yaw % (2 * Math.PI)
+	console.log(yaw)
+	if(yaw < 0)
+		yaw = (2 * Math.PI) - Math.abs(yaw)
+	return yaw;
+}
+
+function isBlockEmpty (pos) {
+	var res = true;
+	unit.forEach(function (vec) {
+		res = res && isEmpty(pos.plus(vec));	
+	});
+	return res;
+}
+
+function freeNeighbors (pos) {
+	var neighbors = [];
+	unit.forEach(function (vec) {
+		if (isEmpty(pos.plus(vec)))
+			neighbors.push(vec);
+	});
+	return neighbors;
+}
+
+function isEmpty (pos) {
+	return bot.blockAt(pos) && bot.blockAt(pos).boundingBox === 'empty' 
+
+}	
+
+function boundingBox (pos) {
+	var block = bot.blockAt(pos)
+	if(block != null)
+		return block.boundingBox;
+}
+
 function randomIntInc (low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
 }
+
+
 
 function getNextBlock (yaw, position) {
 	switch(yaw) {
