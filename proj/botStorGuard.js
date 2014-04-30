6bfca4c35c5e63ae90f13813010b1ba5a7f80153
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2014-04-30 19:28:15 +0100

    Add new bot guards api.
    Fix problem with death of guards

---
+++ proj/botStorGuard.js
@@ -0,0 +1,54 @@
+var mineflayer = require('mineflayer');
+var EventEmitter = require('events').EventEmitter
+var mvc = require('./movementController');
+var vec3 = mineflayer.vec3;
+var cron = require('cron');
+
+var args = process.argv;
+
+var bot = mineflayer.createBot({
+  username: args[2],
+  'spawnPoint': vec3 (parseInt(args[3]), parseInt(args[4]), parseInt(args[5])),
+});
+
+mvc.setBot(bot);
+
+
+var cronJob = cron.job("* * * * * *", function(){
+	try {
+		var enemy = mvc.nearestEntity();
+		//console.log(enemy);
+		if(enemy){
+			bot.lookAt(enemy.position);
+			bot.attack(enemy);
+		}
+	} catch (ex) {
+		console.log ("nheeeee")
+	}
+}); 
+
+cronJob.start();
+
+bot.on('spawn', function(){
+	var item = mvc.itemByName("swordDiamond");
+	if(item != null){
+		bot.equip(item, 'hand', function(err) {
+			if (err) {
+			  bot.chat("unable to equip " + "Diamond Sword");
+			  console.error(err.stack);
+			} else {
+			  bot.chat("equipped " + "Diamond Sword");
+			}
+		})
+	}
+});
+
+
+bot.on('health', function() {
+  bot.chat(args[2] + " have " + bot.health);
+  mvc.listInventory();
+});
+
+bot.on('death', function () {
+	cronJob.stop();
+});
--- proj/botStorGuards.js
+++ proj/botStorGuards.js
@@ -1,80 +1,22 @@
-var mineflayer = require('mineflayer');
-var EventEmitter = require('events').EventEmitter
-var mvc1 = require('./movementController');
-var mvc2 = require('./movementController');
-var vec3 = mineflayer.vec3;
-var cron = require('cron');
+var exec = require('child_process').exec,
+	child1,
+	child2;
 
 
-var bot1 = mineflayer.createBot({
-  username: "guardStor1",
-  'spawnPoint': vec3 (1123, 4, 69),
+child = exec('node botStorGuard.js guardStor1 1123 4 69',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
 });
 
-mvc1.setBot(bot1);
-
-var bot2 = mineflayer.createBot({
-  username: "guardStor2",
-  'spawnPoint': vec3 (1119, 4, 69),
-});
-
-mvc2.setBot(bot2);
-
-
-var cronJob = cron.job("* * * * * *", function(){
-	var enemy = mvc1.nearestEntity();
-	//console.log(enemy);
-		if(enemy){
-			bot1.lookAt(enemy.position);
-			bot1.attack(enemy);
-		}
-
-	var enemy = mvc2.nearestEntity();
-		if(enemy){
-			bot2.lookAt(enemy.position);
-			bot2.attack(enemy);
-		}
-}); 
-
-cronJob.start();
-
-bot1.on('spawn', function(){
-	var item = mvc1.itemByName("swordDiamond");
-	if(item != null){
-		bot1.equip(item, 'hand', function(err) {
-			if (err) {
-			  bot1.chat("unable to equip " + "Diamond Sword");
-			  console.error(err.stack);
-			} else {
-			  bot1.chat("equipped " + "Diamond Sword");
-			}
-		})
-	}
-});
-
-
-bot2.on('spawn', function(){
-	var item = mvc2.itemByName("swordDiamond");
-	if(item != null){
-		bot2.equip(item, 'hand', function(err) {
-			if (err) {
-			  bot2.chat("unable to equip " + "Diamond Sword");
-			  console.error(err.stack);
-			} else {
-			  bot2.chat("equipped " + "Diamond Sword");
-			}
-		});
-	}
-});
-
-
-bot1.on('health', function() {
-  bot1.chat("guardStor1 have " + bot1.health);
-    mvc1.listInventory();
-
-});
-
-bot2.on('health', function() {
-  bot2.chat("guardStor2 have " + bot2.health);
-    mvc2.listInventory();
+child = exec('node botStorGuard.js guardStor2 1119 4 69',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
 });
