f69ef743df5e65eaeabe93ae98e38f3e04b6c013
Ana Rita Pereira <a.ritap@hotmail.com>
2014-05-20 00:49:42 +0100

    added comunications between bots

---
+++ proj/communications.js
@@ -0,0 +1,30 @@
+var mineflayer = require('mineflayer');
+
+var ScoutLJMsg = 
+[
+	'Could you see if there is any wood for me to collect?',
+	'Of course! Let me check',
+	'Yes! There are trees at ',
+	'Thank you! You are my angel', 
+]
+
+var ScoutFoodMsg = 
+[
+	'Could you see if there is any animals for me to kill?',
+	'Of course! Let me check',
+	'Yes! There are animals at ',
+	'Thank you! You are my angel', 
+]
+
+var ConstructorBotsMsgs = []
+
+
+
+
+
+
+
+
+exports.ScoutLJMsg = ScoutLJMsg;
+exports.ScoutFoodMsg = ScoutFoodMsg;
+exports.ConstructorBotsMsgs = ConstructorBotsMsgs;
