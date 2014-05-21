5f5ac1f070bcf9f41ecbf0f820d899e5aacd8d2c
Bia <beatriz.vbernardo@gmail.com>
2014-05-21 18:10:18 +0100

    Built a kitchen and added guards

--- proj/botGuards.js
+++ proj/botGuards.js
@@ -3,13 +3,16 @@ var exec = require('child_process').exec,
 	child2;
 
 // stor guards
-execChild('node botStorGuard.js guardStor1 1123 4 69');
-execChild('node botStorGuard.js guardStor2 1119 4 69');
+execChild('node botStorGuard.js guardStor1 1123 4 70,5');
+execChild('node botStorGuard.js guardStor2 1119 4 70,5');
 
 // village guards
 execChild('node botStorGuard.js guard1 1104 4 140');
 execChild('node botStorGuard.js guard2 1104 4 134');
 
+// kitchen guards
+execChild('node botStorGuard.js guardKitchen1 1131 4 67');
+execChild('node botStorGuard.js guardKitchen2 1131 4 64');
 
 function execChild(process){
 	exec(process,
