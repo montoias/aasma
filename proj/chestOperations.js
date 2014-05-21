5a0a9c4865b4deef302c0396add4b57e2ca772ff
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2014-05-22 00:50:20 +0100

    add some new operations to chest

--- proj/chestOperations.js
+++ proj/chestOperations.js
@@ -3,7 +3,6 @@ var mineflayer = require('mineflayer');
 var vec3 = mineflayer.vec3;
 var bot;
 var c = null;
-var withdrawMaxItems = 60;
 
 function chestPosition () {
 	return vec3(1120,4,59);
@@ -54,33 +53,65 @@ function withdraw (chest, max) {
 	});
 }
 
-function enoughBlocksToWithdraw (chest) {
-	var buildItems = chest.items().sort(sortFunction).filter(function(item) {
+function deposit (chest, itemType) {
+	if(max <= 0) {
+		console.log("Deposited all items");
+		chest.close();
+		setTimeout(function () {bot.emit("DepositComplete");}, 1000);
+		return;
+	}
+
+	var items = chest.items().filter(function(item) {
+	    return itemType[item.type];
+	});
+
+	var item = items[0]
+	chest.deposit(item.type, null, item.amount, function(err) {
+		if (err) {
+		  bot.chat("unable to deposit " + item.count + " " + item.name);
+		} else {
+		  console.log("deposited ",item.amount, item.name)
+		  bot.chat("deposited " + item.amount + " " + item.name);
+		  setTimeout(function () {deposit(chest, itemType)}, 1000);
+		}
+	});
+}
+
+function enoughBlocksToWithdraw (chest, type, maxItems) {
+	var items = chest.items().sort(sortFunction).filter(function(item) {
 	    return buildingTypes[item.type];
 	});
 
-	var amount = itemSize(buildItems);
+	var amount = itemSize(items);
 	console.log("There is", amount, "in the chest! I Can Now start building");
-	if(amount >= withdrawMaxItems){
-		setTimeout(function () {withdraw(chest, withdrawMaxItems);}, 1000)
+	if(amount >= maxItems){
+		setTimeout(function () {withdraw(chest, maxItems);}, 1000)
 	} else {
 		console.log("I will wait for more blocks. there is only", amount);
 		bot.chat("needblocks")
 		chest.close();
 		return;
-	}
-		
+	}	
 }
 
-function moveToAndOpen (pos) {
-	bot.scaffold.to(pos, function(err) {
+//chest TYPE -> (POSICAO DO BOT, POSICAO DA CAIXA)
+var chest = { 'food' : [vec3(1120,4,67),vec3(1119,4,67)],
+	   		  'wood' : [vec3(1122,4,67),vec3(1123,4,67)], 
+			  'other1' : [vec3(1122,4,66),vec3(1122,4,65)], 
+			  'other2' : [vec3(1120,4,66),vec3(1120,4,65)]
+			};
+
+function moveToAndOpen (type) {
+	var res = chest[type];
+ 	
+	bot.scaffold.to(res[0], function(err) {
       if (err) {
-        console.log("didn't make it: " ,err.code, pos);
+        console.log("didn't make it: " ,err.code, res);
       } else {
         bot.chat("made it!");
         console.log("open chest")
-        c = bot.openChest(bot.blockAt(chestPosition()));
-        setTimeout(function  (argument) { enoughBlocksToWithdraw(c);} , 1000);
+        c = bot.openChest(bot.blockAt(res[1]));
+     	bot.emit("chestOpen",c, type);
     }
     });
 }
@@ -96,4 +127,5 @@ function itemSize (items) {
 exports.withdraw = withdraw;
 exports.chestPosition = chestPosition;
 exports.setBot = setBot;
+exports.enoughBlocksToWithdraw = enoughBlocksToWithdraw;
 exports.moveToAndOpen = moveToAndOpen;
