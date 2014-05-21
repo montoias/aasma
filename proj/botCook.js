c1ab6e97f8cc7d8353b7cdaa85a79ab0cf3600bb
Bia <beatriz.vbernardo@gmail.com>
2014-05-22 00:59:18 +0100

    Added cooking steps

--- proj/botCook.js
+++ proj/botCook.js
@@ -2,23 +2,132 @@ var mineflayer = require('mineflayer');
 var EventEmitter = require('events').EventEmitter
 var mvc = require('./movementController');
 var vec3 = mineflayer.vec3;
+var oper = require('./chestOperations');
+var navigatePlugin = require('mineflayer-navigate')(mineflayer);
+var scaffoldPlugin = require('mineflayer-scaffold')(mineflayer);
+var ee = require('event-emitter');
+var emitter = ee({}), listener;
+
+var cooking = false;
+var baking = false;
+
+var wood =  17;
+
+var itemsToCook = [
+  319, // porkchop
+  363, // beef
+  365, // chicken
+  392, //potato
+];
+
+var itemsToBake = [
+  296, //wheat
+  353, //sugar
+  344, //egg
+  335, //milk
+  86 //pumpkin
+];
+
+var fruit = [
+  260, //apple
+  391, //carrot
+  360, //melon
+];
+
+var recipes = [
+  297, //bread
+  354, //cake
+  400  //pumpkin pie
+];
 
 
-var bot = mineflayer.createBot({
+var bot = mineflayer.createBot( {
   username: "avillez",
-  'spawnPoint': vec3 (1121, 4, 89),
+  'spawnPoint': vec3 (1140, 4, 66),
 });
 
 //passes to the movementController info about the bot
 mvc.setBot(bot);
+oper.setBot(bot);
 
-bot.on('spawn', function(){
+// install the plugin
+navigatePlugin(bot);
+scaffoldPlugin(bot);
+
+
+bot.on('spawn', function() {
 	//bot.setControlState('forward', true);
 });
 
-bot.on('time', function(){
-	if(bot.time.day > 0 && bot.time.day < 6000) {
-		bot.setControlState('jump', true);
+bot.on('time', function() {
+	if((bot.time.day > 12000 && bot.time.day < 15000) || (bot.time.day > 19000 && bot.time.day < 21000)) {
+		if(!cooking && !baking) {
+			cook();
+
+
+		}
 	}
-	else bot.setControlState('jump', false);
+	else {
+		if(!baking && !cooking) {
+			bake();
+		}
+	}
 });
+
+function cook() {
+	cooking = true;
+	oper.moveToAndOpen('food');
+
+//cooking = false;
+}
+
+function bake() {
+	//baking = true;
+}
+
+bot.on('chestOpen', function(chest, kind) {
+	if(kind == 'wood') {
+		if(chest.count(wood) > 0) {
+			chest.withdraw(wood, null, chest.count(wood), function(err) {
+				chest.close();
+				bot.emit('gotWood');
+			});
+		}
+	}
+
+	else if(cooking) {
+		for(item in itemsToCook) {
+			if(chest.count(item) > 0) {
+				chest.withdraw(item, null, chest.count(item), function(err) {
+					chest.close();
+					bot.emit('gotIngredients');
+				});
+				break;
+			}
+		}	
+	}
+});
+
+bot.on('gotWood', function() {
+	goBackToKitchen();
+});
+
+bot.on('gotIngredients', function() {
+	if(cooking)
+		oper.moveToAndOpen('wood');
+	else 
+		goBackToKitchen();
+});
+
+function goBackToKitchen () {
+  bot.scaffold.to(vec3 (1140, 4, 66), function(err) {
+    if (err) {
+        console.log("didn't make it: " ,err.code, vec3 (1140, 4, 66), "trying again");
+    } 
+    else {
+        bot.chat("made it!");
+        bot.emit('backToKitchen');
+	}
+    });
+}
+
